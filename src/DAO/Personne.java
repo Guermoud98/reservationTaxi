fc2eef5f5d40041f7e1e7853ebdf447667a01019
Gur <guermoud.@gmail.com>
2023-12-17 16:00:46 +0100

    Added Personne class to the project

---
+++ src/DAO/Personne.java
@@ -0,0 +1,53 @@
+package DAO;
+
+public class Personne {
+    protected int id;
+    protected String nom;
+    protected String prenom;
+    protected String telephone;
+    protected String email;
+
+    public Personne(int id, String nom, String prenom, String telephone, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.email = email;
+    }
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    //setters
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
