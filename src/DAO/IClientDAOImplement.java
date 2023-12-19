9f0195bbda3e17743beebf54e5702b651a9ad108
Gur <guermoud.@gmail.com>
2023-12-19 23:13:49 +0100

    Added methods to verify whether the password is valid or not.

--- src/DAO/IClientDAOImplement.java
+++ src/DAO/IClientDAOImplement.java
@@ -8,9 +8,14 @@ import java.sql.Connection;
 public class IClientDAOImplement implements IClientDAO{
     Connection conn = ConnectionDB.getConnexion();
     public void register(Personne p) {
+        isValidEmail(p.getEmail()); // On fait appel à la fonction isValidEmail pour verifier la validité de l'email
+        isValidPassword(p.getPassword());
+
+    }
+    public void isValidEmail(String email) {
         String regexExpression = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z]{2,})$";
         Pattern pattern = Pattern.compile(regexExpression);
-        Matcher matcher = pattern.matcher(p.getEmail());
+        Matcher matcher = pattern.matcher(email);
         boolean m = matcher.find();
         if(m == true) {
             System.out.println("Valid Email");
@@ -18,7 +23,33 @@ public class IClientDAOImplement implements IClientDAO{
         else {
             System.out.println("Invalid Email");
         }
-
-
+    }
+    public void isValidPassword(String password) {
+        if (password.length()< 12 && passwordContainsLowerLetters(password) > 0 && passwordContainsUpperLetters(password) > 0) {
+            System.out.println("Valid password");
+        }
+        else {
+            System.out.println("Invalid password");
+        }
+    }
+    public int passwordContainsUpperLetters(String password) {
+        int upperCount = 0;
+        for(int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if(Character.isUpperCase(ch)) {
+                upperCount++;
+            }
+        }
+        return upperCount;
+    }
+    public int passwordContainsLowerLetters(String password) {
+        int lowerCount = 0;
+        for(int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if(Character.isLowerCase(ch)) {
+                lowerCount++;
+            }
+        }
+        return lowerCount;
     }
 }
--- src/DAO/IPersonneDAO.java
+++ src/DAO/IPersonneDAO.java
@@ -3,6 +3,10 @@ import Business.*;
 
 public interface IPersonneDAO {
     public void register(Personne p);
+    public void isValidEmail(String email);
+    public void isValidPassword(String password);
+    public int passwordContainsUpperLetters(String password);
+    public int passwordContainsLowerLetters(String password);
     //public void login(String email, String password);
    //public void consulterHistoriqueReservation();
     //public void updateInfo();
