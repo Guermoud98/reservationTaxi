59fcd113790ef79892eea112f1d9471a761d22be
Gur <guermoud.@gmail.com>
2023-12-27 00:31:17 +0100

    Added convertirSrcEnLongLat and convertirDestEnLongLat methods. These methods are invoked in the reserver method to convert the location to longitude and latitude.

--- src/main/java/Business/GestionReservation.java
+++ src/main/java/Business/GestionReservation.java
@@ -22,28 +22,35 @@ public class GestionReservation {
     }
     public void Reserver(String lieuSource, String lieuDestination) {
         /*********Source*******/
+        convertirSrcEnLongLat(lieuSource);
+        convertirDestEnLongLat(lieuDestination);
+
+
+    }
+    //cette methode fait la conversion du lieuSource en latitude et longitude
+    public void convertirSrcEnLongLat(String lieuSource) {
+        /*********Source*******/
         //request
         JOpenCageForwardRequest requestSource = new JOpenCageForwardRequest(lieuSource);
         //response
         JOpenCageResponse responseSource = key.forward(requestSource);
         //firstPosition : // get the coordinate pair of the first result
         JOpenCageLatLng firstResultLatLngSource = responseSource.getFirstPosition();
-        /*********Destination*******/
-        //request
+        /*********Affichage LongLati Source*******/
+        System.out.println("Source info: \n Latitude: "  + firstResultLatLngSource.getLat().toString() + ",Longitude: " + firstResultLatLngSource.getLng().toString() + "\n");
+
+    }
+    //cette methode fait la conversion du lieuDestination en latitude etlongitude
+    public void convertirDestEnLongLat(String lieuDestination) {
         JOpenCageForwardRequest requestDestination = new JOpenCageForwardRequest(lieuDestination);
         //response
         JOpenCageResponse responseDestination = key.forward(requestDestination);
         //firstPosition :  get the coordinate pair of the first result
         JOpenCageLatLng firstResultLatLngDestination = responseDestination.getFirstPosition();
-        /*********Affichage LongLati Source*******/
-        System.out.println("Source info: \n Latitude: "  + firstResultLatLngSource.getLat().toString() + ",Longitude: " + firstResultLatLngSource.getLng().toString() + "\n");
         /*********Affichage LongLati Destination*******/
         System.out.println("Destination info: \n Latitude: "  + firstResultLatLngDestination.getLat().toString() + ",Longitude: " + firstResultLatLngDestination.getLng().toString() + "\n");
 
-
-
-
-
     }
+
 
 }
