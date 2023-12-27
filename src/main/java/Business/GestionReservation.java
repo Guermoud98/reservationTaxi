3e04619df4285ae473c69ae7148ed246082d3902
Gur <guermoud.@gmail.com>
2023-12-28 00:55:04 +0100

    Added modifications to the calculerDistance method

--- src/main/java/Business/GestionReservation.java
+++ src/main/java/Business/GestionReservation.java
@@ -6,6 +6,7 @@ import com.byteowls.jopencage.JOpenCageGeocoder;
 import com.byteowls.jopencage.model.JOpenCageForwardRequest;
 import com.byteowls.jopencage.model.JOpenCageLatLng;
 import com.byteowls.jopencage.model.JOpenCageResponse;
+import java.lang.Math;
 
 import java.util.HashMap;
 
@@ -13,7 +14,7 @@ import java.util.HashMap;
 public class GestionReservation {
     private String lieuDestination;
     private String lieuSource;
-    private JOpenCageGeocoder key = new JOpenCageGeocoder("5b0f324abbff4feda79ea888b6472ae6");
+    private final JOpenCageGeocoder key = new JOpenCageGeocoder("5b0f324abbff4feda79ea888b6472ae6");
 
     public GestionReservation(String lieuDestination, String lieuSource) {
         this.lieuDestination = lieuDestination;
@@ -23,58 +24,55 @@ public class GestionReservation {
 
     }
     public void Reserver(String lieuSource, String lieuDestination) {
-        /*********Source*******/
-        convertirDestSrcEnLongLat(lieuSource, lieuDestination);
+        //la conversion du lieuDestination et lieuSource en latitude et longitude
+        System.out.println("LongitudeSource: " + getLongitudeLieu(lieuSource));
+        System.out.println("LatitudeSource: " + getLatitudeLieu(lieuSource));
+        System.out.println("LongitudeDestination: " + getLongitudeLieu(lieuDestination));
+        System.out.println("LatitudeDestination: " + getLatitudeLieu(lieuDestination));
 
-
+        System.out.println("La distance est " + calculerDistance(lieuSource, lieuDestination));
 
     }
-    //cette methode fait la conversion du lieuSource en latitude et longitude
-    public void convertirDestSrcEnLongLat(String lieuSource, String lieuDestination) {
-        /*********Source*******/
-        //request
-        JOpenCageForwardRequest requestSource = new JOpenCageForwardRequest(lieuSource);
-        //response
-        JOpenCageResponse responseSource = key.forward(requestSource);
-        //firstPosition : // get the coordinate pair of the first result
-        JOpenCageLatLng firstResultLatLngSource = responseSource.getFirstPosition();
-        /*********Affichage LongLati Source*******/
-        System.out.println("Source info: \n Latitude: "  + firstResultLatLngSource.getLat().toString() + ",Longitude: " + firstResultLatLngSource.getLng().toString() + "\n");
-        /*********Destination*******/
-        JOpenCageForwardRequest requestDestination = new JOpenCageForwardRequest(lieuDestination);
-        //response
-        JOpenCageResponse responseDestination = key.forward(requestDestination);
-        //firstPosition :  get the coordinate pair of the first result
-        JOpenCageLatLng firstResultLatLngDestination = responseDestination.getFirstPosition();
-        /*********Affichage LongLati Destination*******/
-        System.out.println("Destination info: \n Latitude: "  + firstResultLatLngDestination.getLat().toString() + ",Longitude: " + firstResultLatLngDestination.getLng().toString() + "\n");
-    }
-    //cette methode fait la conversion du lieuDestination en latitude etlongitude
-
-    //public float calculerDistance()
 
     //cette methode retourne un hashmap qui stock la longitude et sa valeur du lieu passe en parametre
-    public HashMap<String, Float> getLongitudeLieu(String lieu) {
-        HashMap<String, Float> srcLongitude = new HashMap<String, Float>();
+    public float getLongitudeLieu(String lieu) {
         //request
         JOpenCageForwardRequest request = new JOpenCageForwardRequest(lieu);
         //response
         JOpenCageResponse response = key.forward(request);
         //firstPosition : // get the coordinate pair of the first result
         JOpenCageLatLng firstResult = response.getFirstPosition();
-        srcLongitude.put("Longitude", Float.parseFloat(firstResult.getLng().toString())); // on fait le parse car firstResultLatLngSource.getLng().toString() retourne un string
+        float srcLongitude = Float.parseFloat(firstResult.getLng().toString()); // on fait le parse car firstResultLatLngSource.getLng().toString() retourne un string
         return srcLongitude;
     }
-    public HashMap<String, Float> getLatitudeLieu(String lieu) {
-        HashMap<String, Float> srcLatitude = new HashMap<String, Float>();
+    public float getLatitudeLieu(String lieu) {
         //request
         JOpenCageForwardRequest request = new JOpenCageForwardRequest(lieu);
         //response
         JOpenCageResponse response = key.forward(request);
         //firstPosition : // get the coordinate pair of the first result
         JOpenCageLatLng firstResult = response.getFirstPosition();
-        srcLatitude.put("Longitude", Float.parseFloat(firstResult.getLat().toString())); // on fait le parse car firstResult.getLng().toString() retourne un string
+        float srcLatitude = Float.parseFloat(firstResult.getLat().toString()); // on fait le parse car firstResult.getLng().toString() retourne un string
         return srcLatitude;
+    }
+    public double calculerDistance(String lieuSource, String lieuDestination) {
+        double distance = -1 ; //en km
+        try {
+           //les longitudes et latitudes reçus vial'Api sont en degre
+           double srcLongitude = Math.toRadians(getLongitudeLieu(lieuSource));
+           double srcLatitude = Math.toRadians(getLatitudeLieu(lieuSource));
+           double destLongitude = Math.toRadians(getLongitudeLieu(lieuDestination));
+           double destLatitude = Math.toRadians(getLatitudeLieu(lieuDestination));
+           double rayonTerre = 6371;
+           double differenceLongitude = Math.toRadians(destLongitude - srcLongitude);
+           double distanceAngulaire = Math.acos(Math.sin(srcLatitude) * Math.sin(destLatitude) + Math.cos(srcLatitude) * Math.cos(destLatitude) * Math.cos(differenceLongitude));
+           distance = distanceAngulaire * rayonTerre;
+
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+
+        return distance;
     }
 
 }
