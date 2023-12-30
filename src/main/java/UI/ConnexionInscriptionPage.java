ca31155db199ac696f0bb28ff1871ae9620bfd36
Gur <guermoud.@gmail.com>
2023-12-30 17:10:50 +0100

    Added ConnexionIscriptionPage class in the UI package

---
+++ src/main/java/UI/ConnexionInscriptionPage.java
@@ -0,0 +1,60 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ConnexionInscriptionPage extends JFrame {
+    public ConnexionInscriptionPage() {
+        // Configuration de la fenêtre
+        setTitle("Connexion/Inscription");
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Création du panneau principal avec un gestionnaire de disposition FlowLayout
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 20));
+        panel.setBackground(Color.BLACK);  // Fond noir
+        setContentPane(panel);
+
+        // Bouton "Connexion"
+        JButton connexionButton = new JButton("Connexion");
+        styleButton(connexionButton); // Appliquer le style au bouton
+        connexionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Code à exécuter lorsque le bouton "Connexion" est cliqué
+                System.out.println("Bouton Connexion cliqué");
+            }
+        });
+        panel.add(connexionButton);
+
+        // Bouton "Inscription"
+        JButton inscriptionButton = new JButton("Inscription");
+        styleButton(inscriptionButton); // Appliquer le style au bouton
+        inscriptionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Code à exécuter lorsque le bouton "Inscription" est cliqué
+                System.out.println("Bouton Inscription cliqué");
+            }
+        });
+        panel.add(inscriptionButton);
+
+        // Affichage de la fenêtre
+        setVisible(true);
+    }
+
+    private void styleButton(JButton button) {
+        button.setBackground(Color.WHITE);
+        button.setForeground(Color.BLACK);
+        button.setFocusPainted(false); // Pour supprimer le contour de mise au point
+    }
+
+
+}
+
+
+
+
---
+++ src/main/java/UI/Test.java
@@ -0,0 +1,8 @@
+package UI;
+
+public class Test {
+    public static void main (String [] args) {
+        //ChoixRolePage choix = new ChoixRolePage();
+        ConnexionInscriptionPage con = new ConnexionInscriptionPage();
+    }
+}
